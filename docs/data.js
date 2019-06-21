4b03596bf4eb6accb542fbcac66f3c87e42b705b
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-06-21 10:31:23 +0700

    Update description

--- docs/data.js
+++ docs/data.js
@@ -1,49 +1,10 @@
 const projects = [
   {
-    title: "Toko Diba",
-    url: "https://www.tokodiba.com",
-    img: "tokodiba.png",
-    description: {
-      en: "Local business website built with Nuxt and Bootstrap.",
-      id: "Situs usaha lokal yang dibuat dengan Nuxt dan Bootstrap."
-    }
-  },
-  // {
-  //   title: "TechIQ",
-  //   url: "http://techiq.io",
-  //   img: "techiq.jpg",
-  //   description: {
-  //     en: "Software services firm website built with Nuxt and Bootstrap.",
-  //     id:
-  //       "Situs perusahaan layanan perangkat lunak yang dibuat dengan Nuxt dan Bootstrap."
-  //   }
-  // },
-  {
     title: "Amalia Studio",
     url: "https://amalia.netlify.com",
     img: "amalia.png",
     description: {
-      en: "Sample digital agency website built with Vue and Bootstrap.",
-      id: "Contoh situs agensi digital yang dibuat dengan Vue dan Bootstrap."
-    }
-  },
-  {
-    title: "Enlightenment News",
-    url: "https://enlightenment.netlify.com",
-    img: "enlightenment.png",
-    description: {
-      en: "Sample news website built with Nuxt, Guardian's API, and CSSNext.",
-      id:
-        "Contoh situs berita yang dibuat dengan Nuxt, API Guardian, dan CSSNext."
-    }
-  },
-  {
-    title: "Tiara Restaurant",
-    url: "https://tiara.netlify.com",
-    img: "tiara.png",
-    description: {
-      en: "Sample restaurant website built with Vue and Bootstrap.",
-      id: "Contoh situs restoran yang dibuat dengan Vue dan Bootstrap."
+      en: "Sample digital agency website built with Vue and Bootstrap."
     }
   },
   {
@@ -51,8 +12,32 @@ const projects = [
     url: "https://azka.netlify.com",
     img: "azka.png",
     description: {
-      en: "Sample ecommerce website built with Nuxt, Bootstrap, and Snipcart.",
-      id: "Contoh toko daring yang dibuat dengan Nuxt, Bootstrap, dan Snipcart."
+      en: "Sample ecommerce website built with Nuxt, Bootstrap, and Snipcart."
+    }
+  },
+  {
+    title: "Enlightenment News",
+    url: "https://enlightenment.netlify.com",
+    img: "enlightenment.png",
+    description: {
+      en:
+        "Sample news website built with Nuxt, Guardian's API, Fixer API, and PostCSS."
+    }
+  },
+  {
+    title: "Tiara Restaurant",
+    url: "https://tiara.netlify.com",
+    img: "tiara.png",
+    description: {
+      en: "Sample restaurant website built with Vue and Bootstrap."
+    }
+  },
+  {
+    title: "Toko Diba",
+    url: "https://www.tokodiba.com",
+    img: "tokodiba.png",
+    description: {
+      en: "Local business website built with Nuxt and Bootstrap."
     }
   }
 ];
